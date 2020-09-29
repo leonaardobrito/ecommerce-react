67c674ab56db2376a5d210e7924bb3997b24c999
Leonardo <leonardonunesbrito@gmail.com>
2020-09-29 09:20:48 -0300

    backup

---
+++ api/config/multer.js
@@ -0,0 +1,15 @@
+/* 
+ * Upload de arquivos. Filename salva como .jpg
+ * O armazenamento é feito em disco em <dir>/public/images/<filename.jpg>
+ */
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => callback(null, __dirname + '/../public/images'),
+    filename: (req, file, callback) =>(null, file.fieldname + '-' + Date.now() + '.jpg')
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
